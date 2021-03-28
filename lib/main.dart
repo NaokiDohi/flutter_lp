02515487ebd1dcf3fb48adf55dbdeef32f50e0c9
NaokiDohi <1210113.naoki.dohi.913@gmail.com>
2021-03-28 20:02:19 +0900

    Create fotter

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,22 @@ class LandingPage extends StatelessWidget {
               title: document['title'],
               url: document['url'],
             ),
+          Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Made in Flutter',
+                style: TextStyle(fontSize: 19),
+              ),
+              SizedBox(width: 8),
+              Image.asset(
+                "assets/Icon-192.png",
+                width: 25,
+              )
+            ],
+          ),
+          SizedBox(height: 23),
         ],
       ),
     );
