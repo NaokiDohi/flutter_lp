a13ea5493e5a1da6431ca53cf5619eaaed8d8e64
NaokiDohi <1210113.naoki.dohi.913@gmail.com>
2021-03-31 22:07:24 +0900

    Stream Builder to Show Data

--- lib/landing_page/landing_page.dart
+++ lib/landing_page/landing_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_lp/landing_page/button_link.dart';
 import 'package:flutter_lp/constant.dart';
@@ -8,34 +9,38 @@ class LandingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        children: [
-          SizedBox(height: 35),
-          CircleAvatar(
-            backgroundImage: NetworkImage(imageUrl),
-            backgroundColor: Colors.white,
-            radius: 48,
-          ),
-          SizedBox(height: 12),
-          Text(
-            "@social handle",
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.grey,
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('links').snapshots(),
+        builder: (context, snapshot) {
+          return Material(
+            child: Column(
+              children: [
+                SizedBox(height: 35),
+                CircleAvatar(
+                  backgroundImage: NetworkImage(imageUrl),
+                  backgroundColor: Colors.white,
+                  radius: 48,
+                ),
+                SizedBox(height: 12),
+                Text(
+                  "@social handle",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(height: 20),
+                for (var document in documents)
+                  ButtonLink(
+                    title: document.title,
+                    url: document.url,
+                  ),
+                Spacer(),
+                Footer(),
+                SizedBox(height: 23),
+              ],
             ),
-          ),
-          SizedBox(height: 20),
-          for (var document in documents)
-            ButtonLink(
-              title: document.title,
-              url: document.url,
-            ),
-          Spacer(),
-          Footer(),
-          SizedBox(height: 23),
-        ],
-      ),
-    );
+          );
+        });
   }
 }
